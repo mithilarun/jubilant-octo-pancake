ab4eed87d81fadfd7be6bfaffd3fb52276976c89
Ambarish Ravindran <aravin10@asu.edu>
2016-10-30 15:59:36 -0700

    Added the hash computation function

---
+++ inc/octo_const.h
@@ -0,0 +1,33 @@
+#ifndef CONST_H
+#define CONST_H
+
+/*
+ * Copyright (C) 2016, Mithil Arun
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+int H0 = 0x6a09e667
+int H1 = 0xbb67ae85
+int H2 = 0x3c6ef372
+int H3 = 0xa54ff53a
+int H4 = 0x510e527f
+int H5 = 0x9b05688c
+int H6 = 0x1f83d9ab
+int H7 = 0x5be0cd19
+
+#endif
+
+
---
+++ src/octo_compute.c
@@ -0,0 +1,82 @@
+
+/*
+ * Copyright (C) 2016, Mithil Arun
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "octo_const.h"
+#include "octo_utils.h"
+
+void 
+octo_hash_compute(octo_mesg_t *msg)
+{
+	int *wt;
+	int N;
+	int i,iter = 0, count;
+	int a,b,c,d,e,f,g,h;
+	int T1, T2;
+
+	wt = (int*)malloc(sizeof(int)*64);
+	N = (msg->buf_sz/64);
+
+
+	for(i = 0; i < N; i++)
+	{
+		count  = 0;
+		do {
+			wt[count] = msg->msg[iter];
+			iter++;
+			count++;
+		} while((iter % 16) != 0);
+
+		for (count = 16; count < 64; count++) {
+			wt[count] = octo_sig4(wt[count-2]) + wt[count-7] +
+			    octo_sig3(wt[count-15]) + wt[count-16];
+		}
+
+		a = H0;
+		b = H1;
+		c = H2;
+		d = H3;
+		e = H4;
+		f = H5;
+		g = H6;
+		h = H7;
+
+		for(t = 0; t < 64; t++){
+			T1 = h + octo_sig2(e) + ch(e, f, g) + K[t] + wt[t];
+			T2 = octo_sig1(a) + maj(a, b, c);
+			h = g;
+			g = f;
+			f = e;
+			e = d + T1; 
+			d = c;
+			c = b;
+			b = a;
+			a = T1 + T2;
+		}
+		
+		H0 = a + H0;
+		H1 = b + H1;
+		H2 = c + H2;
+		H3 = d + H3;
+		H4 = e + H4;
+		H5 = f + H5;
+		H6 = g + H6;
+		H7 = h + H7;
+	}
+}
+
+
